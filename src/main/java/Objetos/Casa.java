fa0280f8453367992547460bd691cb09a602e870
jangarstud <72555163+jangarstud@users.noreply.github.com>
2021-01-04 17:20:02 +0100

    Documentación final restante

--- src/main/java/Objetos/Casa.java
+++ src/main/java/Objetos/Casa.java
@@ -88,6 +88,16 @@ public class Casa {
         return false;
     }
 
+    /**
+     * Método que permite eliminar una lista de objetos de la casa.
+     *
+     * En este método se implementa la operación para poder eliminar una lista de objetos deseada de la lista de objetos
+     * en el hogar hasta el momento. Para ello se recorre secuencialmente la lista como parámetro y la lista de la clase
+     * de tal forma que se comparen los objetos por ID para poder proceder al borrado de estos. Una vez realizada la operación
+     * se informa del resultado a través de un valor booleano.
+     * @param listaObjetosAEliminar La lista de objetos que se desea eliminar de la lista de objetos en el hogar.
+     * @return Un estado booleano informando de la satisfactoriedad de la operación.
+     */
     public boolean eliminar_objetos(List<Objeto_Casa> listaObjetosAEliminar){
         for(Objeto_Casa objetoAEliminar : listaObjetosAEliminar){
             for(Objeto_Casa objetoABuscar : objetos){
@@ -100,6 +110,15 @@ public class Casa {
         return false;
     }
 
+    /**
+     * Método que permite actualizar un objeto en el hogar.
+     *
+     * En este método se permite actualizar los datos de un objeto, de tal modo que usando un objeto de entrada se
+     * transfieran todos los valores de sus propiedades al objeto con un mismo ID presente en la lista de objetos
+     * en el hogar. Una vez realizada la operación se informa del resultado a través de un valor booleano.
+     * @param nuevo_valor_objeto El objeto con la información que se desea actualizar.
+     * @return Un estado booleano informando de la satisfactoriedad de la operación.
+     */
     public boolean actualizar_objeto(Objeto_Casa nuevo_valor_objeto){
         for (Objeto_Casa objeto : objetos) {
             if (objeto.getID().equals(nuevo_valor_objeto.getID())) {
@@ -110,6 +129,15 @@ public class Casa {
         return false;
     }
 
+    /**
+     * Método que permite mostrar un objeto del hogar.
+     *
+     * En este método se devuelve un vector de cadenas con la información correspondiente a cada objeto presente
+     * en la lista actual de objetos en el hogar. Para ello se indica el índice del objeto que se desea mostrar
+     * y posteriormente se simula el desplazamiento de posición y se devuelve la información para ese objeto.
+     * @param i Un entero. El índice del objeto en la lista de objetos en el hogar sobre el que se desea conocer su información.
+     * @return Un vector de String's con la información sobre el elemento deseado.
+     */
     public String[] mostrar_objeto(int i){
         for (Objeto_Casa objeto : objetos){
             objeto.calculate_next_pos(ancho, largo);
